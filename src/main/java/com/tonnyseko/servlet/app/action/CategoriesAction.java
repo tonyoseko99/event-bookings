5fc01192f738190690b3c32f2711df5b04e6e8c3
Tonny Oseko <tonnyoseko0@gmail.com>
2023-12-08 11:10:47 +0300

    categorize the events

--- src/main/java/com/tonnyseko/servlet/app/action/CategoriesAction.java
+++ src/main/java/com/tonnyseko/servlet/app/action/CategoriesAction.java
@@ -1,9 +1,11 @@
 package com.tonnyseko.servlet.app.action;
 
 import com.tonnyseko.servlet.app.bean.EventBean;
+import com.tonnyseko.servlet.app.bean.EventInterface;
 import com.tonnyseko.servlet.app.model.entity.Event;
 import com.tonnyseko.servlet.app.model.enums.CategoryStatus;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,8 +14,10 @@ import java.io.IOException;
 
 @WebServlet("/categories")
 public class CategoriesAction extends BaseAction {
+    @Inject
+    private EventInterface eventBean;
+
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EventBean eventBean = new EventBean();
 
         renderPage(request, response, 2, Event.class, eventBean.getEventsByCategory(CategoryStatus.valueOf(request.getParameter("category").toUpperCase())));
 
---
+++ src/main/java/com/tonnyseko/servlet/app/helpers/HtmlTableColumn.java
@@ -0,0 +1,14 @@
+package com.tonnyseko.servlet.app.helpers;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface HtmlTableColumn {
+
+    String label () default "";
+    
+}
