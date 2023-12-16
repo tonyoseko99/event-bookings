84529365d61bd8eaf6fc7880e30b0d9889ab613c
Tonny Oseko <tonnyoseko0@gmail.com>
2023-12-16 17:44:12 +0300

    fetch the reservations made by a logged in user

--- src/main/java/com/tonnyseko/servlet/app/bean/ReservationI.java
+++ src/main/java/com/tonnyseko/servlet/app/bean/ReservationI.java
@@ -1,8 +1,12 @@
 package com.tonnyseko.servlet.app.bean;
 
+import java.util.List;
+
 import com.tonnyseko.servlet.app.model.entity.Reservation;
 
 public interface ReservationI extends GenericBeanI<Reservation> {
 
     Reservation findById(Long id);
+
+    List<Reservation> getReservationsByUserId(Long userId);
 }
